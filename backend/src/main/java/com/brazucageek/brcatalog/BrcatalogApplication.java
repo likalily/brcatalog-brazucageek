e98fce4500a57b900a54f239f15064a05db69503
Eliane Leite Andrade <elianeleiteandrade@yahoo.com.br>
2023-01-07 20:37:47 -0300

    Backend project created

---
+++ backend/src/main/java/com/brazucageek/brcatalog/BrcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.brazucageek.brcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brazucageek/brcatalog/BrcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazucageek.brcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
